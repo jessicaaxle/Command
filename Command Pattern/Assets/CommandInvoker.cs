a349db0ce15a472044d93212ec385963895bd430
Jessica Le <jessicale422@gmail.com>
2019-10-04 23:55:50 -0400

    Command Pattern
    it works :)

---
+++ Command Pattern/Assets/CommandInvoker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+    static Queue<ICommand> commonBuffer;
+
+    static List<ICommand> commandHistory;
+    static int counter;
+
+    private void Awake()
+    {
+        commonBuffer = new Queue<ICommand>();
+        commandHistory = new List<ICommand>();
+    }
+
+    public static void AddCommand(ICommand command)
+    {
+            while (commandHistory.Count > counter)
+            {
+            commandHistory.RemoveAt(counter);
+            }
+        commonBuffer.Enqueue(command);
+    }
+
+    void Update()
+    {
+        if (commonBuffer.Count > 0)
+        {
+            ICommand c = commonBuffer.Dequeue();
+            c.Execute();
+
+            commandHistory.Add(c);
+            counter++;
+        }
+        else
+            if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (counter > 0)
+            {
+                counter--;
+                commandHistory[counter].Undo();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (counter < commandHistory.Count)
+            {
+                commandHistory[counter].Execute();
+                counter++;
+            }
+        }
+       
+    }
+}
